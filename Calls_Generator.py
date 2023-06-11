faf88dc646e3288bfec224c57a705a666e7e02ea
TimurSA <sayranov.timur@gmail.com>
2023-06-11 20:04:20 +0500

    penis

--- Calls_Generator.py
+++ Calls_Generator.py
@@ -5,8 +5,8 @@ from Elevator import Call, Elevator
 
 
 def generate_call(people):
-    fr = random.randint(-5, 110)
-    to = random.randint(-5, 110)
+    fr = random.randint(1, Elevator.MAX_PEOPLE)
+    to = random.randint(1, Elevator.MAX_PEOPLE)
     pl = people
 
     return Call(from_floor=fr, to_floor=to, people=pl)
